ec295926ddb8e24f53870c1287279f932c3cc290
Jerrod Smith <VexorHoldings@outlook.com>
2021-12-04 00:13:59 -0500

    Implement all feature post unit testing

--- SymbolData.py
+++ SymbolData.py
@@ -7,10 +7,22 @@ class SymbolData:
         self.Fast = None
         self.Slow = None
         self.Trend = None
+        self.AverageTrueRange = None
+        self.TriggerBarATR = None
+        self.StopLevel = None
+        self.PredictionInterval = None
+        self.QuoteBarWindow = {}
         self.FastIsOverSlow = False
         self.IsAboveTrendLine = False
-        self.PreviousDirection = None
+        self.Direction = None
+
+    def ResetComparators(self):
+        #Has fast MA crossed over slow MA
+        self.FastIsOverSlow = self.Fast > self.Slow
         
+        #Are fast and slow MA above the trend line
+        self.IsAboveTrendLine = self.Fast > self.Trend and self.Slow > self.Trend  
+
     @property
     def SlowIsOverFast(self):
         return not self.FastIsOverSlow
@@ -18,8 +30,5 @@ class SymbolData:
     @property
     def IsBelowTrendLine(self):
         return not self.IsAboveTrendLine
-    
-    def Reset(self):
-        self.Fast.Reset()
-        self.Slow.Reset()
-        self.Trend.Reset()
+
+
--- main.py
+++ main.py
@@ -1,103 +1,170 @@
 from datetime import timedelta
 from AlgorithmImports import *
+from QuantConnect.Algorithm.Framework.Alphas.Analysis import InsightManager
 from Execution.InstantExecutionModel import InstantExecutionModel 
-from AlphaCreation.MACrossMAFilterAlphaModel import MACrossMAFilterAlphaModel
 from PortfolioConstruction.StaticWeightPortfolioConstructionModel import StaticWeightPortfolioConstructionModel
+from SymbolData import SymbolData
+
 
 class sutulpy(QCAlgorithm):  
     def Initialize(self):            
         #Set backtesting variables
         initialCash = 100000
-        self.SetStartDate(2010, 1, 1)
+        self.SetStartDate(2018, 1, 1)
         self.SetEndDate(2020, 1, 1)
         self.SetCash(initialCash)
         self.SetWarmUp(timedelta(days = 20))
 
-        #Set Universe Selection
-        self.universe = []
-        self.USDJPY = self.AddForex('USDJPY', Resolution.Hour)
-        self.SetUniverseSelection(ManualUniverseSelectionModel(self.USDJPY))
+        #Build a universe set to send to ManualUniverseSelectionModel 
+        self.universe = {}       
+        self.resolution = Resolution.Daily
+        self.USDJPY = self.AddForex('USDJPY', Resolution.Hour).Symbol
+        universeSelection = {
+            self.USDJPY
+        }
+        self.SetUniverseSelection(ManualUniverseSelectionModel(universeSelection))
         
-        #Alpha Creation
-        self.AddAlpha(MACrossMAFilterAlphaModel(
-            fastPeriod = 25,
-            slowPeriod = 50,
-            trendPeriod = 200,
-            resolution = Resolution.Daily,
-            predictionResolution = Resolution.Hour,
-            predictionPeriod = 1,
-            movingAverageType = MovingAverageType.Exponential)) 
-        
+        #Alpha Creation        
+        self.fastPeriod = 25
+        self.slowPeriod = 50
+        self.trendPeriod = 200
+        self.movingAverageType = MovingAverageType.Exponential
+
         #Portfolio Construction
-        self.SetPortfolioConstruction(StaticWeightPortfolioConstructionModel(
-            portfolioBias = PortfolioBias.LongShort,
-            positionWeight = 0.01
-        ))
+        self.positionSize = initialCash * .01
 
         #Risk Management
-        #Register ATR Indicator
-        symbol = self.USDJPY.Symbol
-        self.symbolQuoteBarWindow = {}
-        self.symbolStopLevel = {}
-        self.symbolTriggerBarATR = {}
-        self.symbolRunningATR = {}
-        self.atrMultiplier = 1
-        self.atr = self.ATR(symbol, 14, MovingAverageType.Simple, Resolution.Daily)
-        self.RegisterIndicator(symbol, self.atr, Resolution.Daily)
-        self.symbolQuoteBarWindow[symbol.Value] = RollingWindow[QuoteBar](3)
-        self.SetRiskManagement(NullRiskManagementModel())
+        self.atrMultiplier = 1        
         
-        #Execution       
-        self.SetExecution(InstantExecutionModel()) 
 
-    
+    # This method gets called at the end of each time slice. A time slice will cover
+    # the last available time period of the lowest data resolution subscription 
     def OnData(self, data):  
+        #Skip warm up period
         if self.IsWarmingUp: return
 
-        # Manage all current securities        
+        # Manage risk on all current securities        
         for kvp in self.Securities:
             # Get underlying symbol  
             symbol = kvp.Key
-            
-            # Get stop level if we have it
-            stopLevel = 0.0
-            if symbol in self.symbolStopLevel:
-                stopLevel = self.symbolStopLevel[symbol]
+            security = kvp.Value
+
+            #Get symbol data
+            symbolData = self.universe[symbol]
+            if symbolData is None:
+                symbolData = SymbolData(security)
 
             # If data slice contains this symbol
-            if data.ContainsKey(symbol):           
-                # Add QuoteBar to RollingWindow collection     
-                self.symbolQuoteBarWindow[symbol.Value].Add(data[symbol])
+            if data.ContainsKey(symbol):   
+                # Add QuoteBar to RollingWindow collection 
+                symbolData.QuoteBarWindow[symbol].Add(data[symbol])
 
-                # Initialize trigger bar for symbol
-                if not symbol in self.symbolTriggerBarATR:
-                    atrValue = self.atr.Current.Value * self.atrMultiplier
-                    self.symbolTriggerBarATR[symbol.Value] = atrValue
+                # Initialize trigger bar for symbol on first available data slice, only runs once per symbol instance
+                if symbolData.TriggerBarATR is None and symbolData.AverageTrueRange.IsReady:
+                    symbolData.TriggerBarATR = symbolData.AverageTrueRange.Current.Value * self.atrMultiplier
 
-                # Initialize stop level for symbol
-                if not symbol in self.symbolStopLevel:
-                    stopLevel = data[symbol].Low - self.symbolTriggerBarATR[symbol.Value]
+                # Initialize stop level for symbol on first available data slice, only runs once per symbol instance
+                if symbolData.StopLevel is None and symbolData.TriggerBarATR is not None:
+                    symbolData.StopLevel = data[symbol].Low - symbolData.TriggerBarATR
 
                 # Only apply lookback logic if we have enough records
-                if self.symbolQuoteBarWindow[symbol.Value].Count is 3:    
-                    onePeriodBackBar = self.symbolQuoteBarWindow[symbol.Value][1]  
-                    twoPeriodBackBar = self.symbolQuoteBarWindow[symbol.Value][2]                     
-
+                if symbolData.QuoteBarWindow[symbol].Count is 3:
                     # If the new low on the previous candle is higher than the low before that,
                     # then the stop level calculation will use this new low
-                    if onePeriodBackBar.Low > twoPeriodBackBar.Low:      
-                        stopLevel = onePeriodBackBar.Low - self.symbolTriggerBarATR[symbol.Value]    
+                    if symbolData.QuoteBarWindow[symbol][1].Low > symbolData.QuoteBarWindow[symbol][2].Low:   
+                        if symbolData.TriggerBarATR is not None:   
+                            symbolData.StopLevel = symbolData.QuoteBarWindow[symbol][1].Low - symbolData.TriggerBarATR    
 
                     # If current price crosses over the stop level, liquidate
-                    if self.Securities[symbol].Price <= stopLevel:
-                        self.Liquidate(symbol)
+                    if symbolData.StopLevel is not None:
+                        if self.Securities[symbol].Price <= symbolData.StopLevel:
+                            self.Liquidate(symbol)                 
+            else:
+                pass
 
-                    # Add/update stop level in dictionary
-                    self.symbolStopLevel[symbol.Value] = stopLevel              
-                else:
-                    pass
-
+            #Emit insights for each symbol in the current universe dictionary
+            #Ensure indicators are ready
+            if symbolData.Fast is not None and symbolData.Slow is not None and symbolData.Trend is not None and symbolData.AverageTrueRange is not None:
+                if symbolData.Fast.IsReady and symbolData.Slow.IsReady and symbolData.Trend.IsReady and symbolData.AverageTrueRange.IsReady:                
+                    #If we do not have a direction value yet, create one. If we do have data, grab it
+                    if symbolData.Direction is None:
+                        symbolData.Direction = InsightDirection.Flat
                 
+                    #Reset comparators for next step
+                    symbolData.ResetComparators()
+
+                    #If fast MA has crossed slow MA and is above the trend line
+                    if symbolData.FastIsOverSlow and symbolData.IsAboveTrendLine:
+                        if symbolData.Direction is InsightDirection.Up:
+                            symbolData.Direction = InsightDirection.Up
+                    #If slow MA has crossed fast MA and is below the trend line
+                    elif symbolData.SlowIsOverFast and symbolData.IsBelowTrendLine:
+                        if symbolData.Direction is not InsightDirection.Down:
+                            symbolData.Direction = InsightDirection.Down
+
+                    #If we are not invested
+                    if not security.Invested:
+                        #Create market order for this symbol
+                        quantity = (self.positionSize / symbolData.Security.Price) * symbolData.Direction
+                        self.MarketOrder(symbol, quantity)
+
+                else:
+                    pass        
+
+                # Add/update symbol data in dictionary
+                self.universe[symbol] = symbolData    
+
+
+    def OnSecuritiesChanged(self, changes):
+        #Clean up removed securities
+        universe = None
+        for removed in changes.RemovedSecurities:
+            #If this symbol exists in the universe collection, remove it
+            if removed.Symbol in self.universe:
+                #Confirm this symbol does not exist in any Universe
+                if not self.UniverseManager.TryGetValue(removed.Symbol, universe):
+                    #Remove symbol from universe
+                    self.universe.pop(removed.Symbol)
             else:
                 continue
+
+        #Create symbol data for new securities
+        for added in changes.AddedSecurities:
+            #If we have already initialized this symbol, move on
+            if added.Symbol in self.universe:
+                continue
+
+            #Initialize symbol data
+            symbolData = SymbolData(added)
+
+            #Set fast, slow, and trend moving average indicators
+            if(self.movingAverageType == MovingAverageType.Simple):
+                symbolData.Fast = self.SMA(added.Symbol, self.fastPeriod, self.resolution)
+                symbolData.Slow = self.SMA(added.Symbol, self.slowPeriod, self.resolution)
+                symbolData.Trend = self.SMA(added.Symbol, self.trendPeriod, self.resolution)
+            elif(self.movingAverageType == MovingAverageType.Exponential):
+                symbolData.Fast = self.EMA(added.Symbol, self.fastPeriod, self.resolution)
+                symbolData.Slow = self.EMA(added.Symbol, self.slowPeriod, self.resolution)
+                symbolData.Trend = self.EMA(added.Symbol, self.trendPeriod, self.resolution)
+            elif(self.movingAverageType == MovingAverageType.DoubleExponential):
+                symbolData.Fast = self.DEMA(added.Symbol, self.fastPeriod, self.resolution)
+                symbolData.Slow = self.DEMA(added.Symbol, self.slowPeriod, self.resolution)
+                symbolData.Trend = self.DEMA(added.Symbol, self.trendPeriod, self.resolution)
+            elif(self.movingAverageType == MovingAverageType.TripleExponential):
+                symbolData.Fast = self.TEMA(added.Symbol, self.fastPeriod, self.resolution)
+                symbolData.Slow = self.TEMA(added.Symbol, self.slowPeriod, self.resolution)
+                symbolData.Trend = self.TEMA(added.Symbol, self.trendPeriod, self.resolution)
+            else:
+                symbolData.Fast = self.EMA(added.Symbol, self.fastPeriod, self.resolution)
+                symbolData.Slow = self.EMA(added.Symbol, self.slowPeriod, self.resolution)
+                symbolData.Trend = self.EMA(added.Symbol, self.trendPeriod, self.resolution)    
+
+            #Initialize AverageTrueRange indicator
+            symbolData.AverageTrueRange = self.ATR(added.Symbol, 14, MovingAverageType.Wilders, self.resolution)
+
+            #Initialize symbol data properties
+            symbolData.QuoteBarWindow[added.Symbol] = RollingWindow[QuoteBar](3)
+
+            #Add this symbol data to the universe dictionary
+            self.universe[added.Symbol] = symbolData
 
